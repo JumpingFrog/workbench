25c596da18299756d63a8061ba13c63549b801ee
David Wotherspoon <davidwother@hotmail.co.uk>
2014-02-16 15:12:22 +0000

    SYAC [Broken]

--- syac/practical3.hs
+++ syac/practical3.hs
@@ -20,15 +20,21 @@ infixl 4 <> -- infix, left associative, priority 4
 			| otherwise = g ts --else return g to ts.
 			where fts@(b,_) = f ts
 --End slide code.
-itr :: Eq t => Parser t -> Parser t
-itr _ [] = (False, [])
-itr p s@(_:[]) = (p s) --one only.
-itr p s@(x1:x2:_)	| fb && sb = itr p (tail s) --x2 will be true, itr further
+itr' :: Eq t => Parser t -> Parser t
+itr' _ [] = (False, [])
+itr' p s@(_:[]) = (p s) --one only.
+itr' p s@(x1:x2:_)	| fb && sb = itr' p (tail s) --x2 will be true, itr further
 					| fb = (True, tail s) --x1 was only true, return here
 					| otherwise = (False, s) --whole string failed
 					where
 					(fb,_) = (p [x1])
 					(sb,_) = (p [x2])
+
+itr :: Eq t => Parser t -> Parser t --parser iterator
+itr _ [] = (False, [])
+itr p s | (length s) == (length r) = (False, s)
+		| otherwise = (True, r)
+		where r = dropWhile (\x->(fst (p [x]))) s
 		
 --1
 empty :: Parser t
@@ -37,7 +43,7 @@ empty ts = (False, ts)
 
 --2.1
 v :: Parser Char
-v = foldl1 (\p n-> (p <> n)) (map (\i->terminal i) ['a'..'z'])
+v = itr (foldl1 (\p n-> (p <> n)) (map (\i->terminal i) ['a'..'z']))
 
 p :: Parser Char
 p = (terminal 'T') <> (terminal 'F') <> v <> ((terminal '-') +> p) <>
@@ -46,14 +52,19 @@ p = (terminal 'T') <> (terminal 'F') <> v <> ((terminal '-') +> p) <>
 t2a = "-(x*(--F*y))"
 t2b = "-(T*(-F*))"
 --2.2
-data PTok = T | F | Var String | CON | NEG | LEFT | RIGHT  deriving (Eq, Show)
---v2 :: Parser PTok
---v2 (Var x) = foldl1 (\p n-> (p <> n)) (map (\i->terminal i) ['a'..'z'])
---v2 x = (False, x) 
+data PTok = T | F | Var String | CON | NEG | LEFT | RIGHT | DIS | IMP | EQL deriving (Eq, Show) --extended for fuller p (3)
+v2 :: Parser PTok
+v2 ((Var x):r)	| a && (b == []) = (True, r)
+		   		| otherwise = error ("Invalid variable name: " ++ x)
+				where (a, b) = v x
+v2 x = (False, x) 
 
 p2 :: Parser PTok
-p2 = (terminal T) <> (terminal F) <> ((terminal NEG) +> p2) <>
+p2 = (terminal T) <> (terminal F) <> ((terminal NEG) +> p2) <> v2 <>
 	 ((terminal LEFT) +> p2 +> (terminal CON) +> p2 +> (terminal RIGHT))
+
+t22a = [NEG, LEFT, Var "x", CON, LEFT, NEG, NEG, F, CON, Var "y", RIGHT, RIGHT] --(x*(--F*y))"
+t22b = [NEG, LEFT, T, CON, LEFT, NEG, F, CON, RIGHT, RIGHT] --"-(T*(-F*))"
 --3
 -- p ::= T | F | v | -p | p * p | p + p | p > p | p = p | (p)
 -- v ::= [a-z]+
@@ -92,3 +103,38 @@ p2 = (terminal T) <> (terminal F) <> ((terminal NEG) +> p2) <>
 --p` ::= ε | =p1p`
 --
 --v ::= [a-z]+
+nil :: Parser PTok
+nil xs = (False, xs)
+
+o5 :: Parser PTok
+o5 = ((terminal LEFT) +> o +> (terminal RIGHT)) <> (terminal T) <> (terminal F) <> v2
+
+o4 :: Parser PTok
+o4 = o5 <> ((terminal NEG) +> o4)
+
+o3 :: Parser PTok
+o3 = o4 +> o3'
+
+o3' :: Parser PTok
+o3' = nil <> ((terminal CON) +> o3 +> o2')
+
+o2 :: Parser PTok
+o2 = o3 +> o2'
+
+o2' :: Parser PTok
+o2' = nil <> ((terminal DIS) +> o3 +> o2')
+
+o1 :: Parser PTok
+o1 = o2 +> o1'
+
+o1' :: Parser PTok --aka r
+o1' = nil <> ((terminal IMP) +> o1)
+
+o :: Parser PTok
+o = o1 +> o'
+
+o' :: Parser PTok
+o' = nil <> ((terminal EQL) +> o1 +> o')
+
+t3a = [NEG, LEFT, Var "x", NEG, NEG, F, CON, LEFT, T, DIS, F, RIGHT, RIGHT]
+t3b = [LEFT, T, CON, NEG, F, CON, NEG]
